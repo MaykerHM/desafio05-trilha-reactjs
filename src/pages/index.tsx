fbfb03b830c9bde3652556085c315fd627075952
MaykerHM <hydeki8@gmail.com>
2021-04-06 20:51:03 -0300

    prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from 'next';
+import Prismic from '@prismicio/client'
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,13 +25,59 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination: { next_page, results } }: HomeProps) {
+  return (
+    <>
+      <main>
+        {
+          results.map(post => {
+            return (
+              <div key={ post.uid }>
+                <h1>{ post.data.title }</h1>
+                <h2>{ post.data.subtitle }</h2>
+                <div>
+                  <p>{ post.first_publication_date }</p>
+                  <p>{ post.data.author }</p>
+                </div>
+              </div>
+            )
+          })
+        }
+      </main>
+    </>
+  )
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query([
+    Prismic.predicates.at('document.type', 'posts')
+  ], {
+    fetch: ['posts.title', 'posts.content']
+  });
 
-//   // TODO
-// };
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: new Date(post.first_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      }),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author
+      }
+    }
+  })
+
+  return {
+    props: {
+      postsPagination: {
+        next_page: '',
+        results: posts,
+      }
+    }
+  }
+};
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,4 +1,6 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { RichText } from 'prismic-dom';
+import Prismic from '@prismicio/client'
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -26,20 +28,72 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({ post }: PostProps) {
+  return (
+    <>
+      <main>
+        <article>
+          <img src={ post.data.banner.url } alt={ post.data.title } />
+          <h1>{ post.data.title }</h1>
+          <div>
+            <p>{ post.first_publication_date }</p>
+            <p>{ post.data.author }</p>
+          </div>
+          {
+            post.data.content.map(content => {
+              return (
+                <div>
+                  <h2>{ RichText.asText(content.heading) }</h2>
+                  <p>{ RichText.asText(content.body) }</p>
+                </div>
+              )
+            })
+          }
+        </article>
+      </main>
+    </>
+  )
+}
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+export const getStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  const posts = await prismic.query([
+    Prismic.predicates.at('document.type', 'posts')
+  ], {
+    fetch: ['posts.slug']
+  });
 
-//   // TODO
-// };
+  const paths = posts.map(
+    post => {
+      return {
+        params: { slug: post.data.slug }
+      }
+    }
+  )
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+  return {
+    paths, fallback: false
+  }
+};
 
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params
+  const prismic = getPrismicClient();
+  const response = await prismic.getByUID('posts', String(slug), {});
+
+  const post = {
+    first_publication_date: new Date(response.first_publication_date).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric'
+    }),
+    data: {
+      title: RichText.asText(response.data.title),
+      banner: { url: response.url },
+      author: RichText.asText(response.data.author),
+      content: response.data.content,
+    },
+  }
+
+  return { props: { post } }
+};
